20be21249e615243716c90462f31fd1d47a164ad
zhangzhenfeng <981831456@qq.com>
2019-07-03 10:33:20 +0800

    js调Android方法进行跳转

---
+++ jsandandroid/src/main/java/com/example/jsandandroid/MainActivity.java
@@ -0,0 +1,210 @@
+package com.example.jsandandroid;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_showmsg;
+    private WebView webview;
+    private Button tv_androidcalljs,tv_androidcalljsargs;
+    Handler handler = new Handler();
+
+    @SuppressLint("JavascriptInterface")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initWebView();
+        initListener();
+
+        //webview.loadUrl("http://baidu.com");
+        webview.loadUrl("file:///android_asset/123.html");
+        //webview.loadUrl("http://192.168.1.121:8889/third_pay/showbutton.html");
+        webview.addJavascriptInterface(MainActivity.this,"jsCallAndroid");
+
+    }
+
+    private void initListener() {
+        tv_androidcalljs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webview.loadUrl("javascript:javacalljs()");
+            }
+        });
+        tv_androidcalljsargs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webview.loadUrl("javascript:javacalljswith(" + "'Android传过来的参数'" + ")");
+            }
+        });
+    }
+
+    private void initWebView() {
+        /*WebSettings webSettings = webview.getSettings();
+        //与js交互必须设置
+        webSettings.setJavaScriptEnabled(true);*/
+
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setDefaultTextEncodingName("utf-8");
+        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        //若加载的html里有JS在执行动画等操作，会造成资源浪费（CPU、电量）在onStop和onResume
+        //里分别把 setJavaScriptEnabled()给设置成false和true即可
+        webSettings.setJavaScriptEnabled(true);
+        // 设置允许JS弹窗
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        //设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        //缩放操作
+        //webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面二个的前提。
+        //webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        //webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        //设置图片加载
+        webSettings.setBlockNetworkImage(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        webSettings.setDomStorageEnabled(true); // 开启 DOM storage API 功能
+        webSettings.setDatabaseEnabled(true);   //开启 database storage API 功能
+        //String cacheDirPath = getFilesDir().getAbsolutePath() + APP_CACAHE_DIRNAME;
+        //webSettings.setAppCachePath(cacheDirPath); //设置  Application Caches 缓存目录
+        webSettings.setAppCacheEnabled(false);//开启 Application Caches 功能
+        //设置WebView是否使用其内置的变焦机制，该机制结合屏幕缩放控件使用，默认是false，不使用内置变焦机制。
+        webSettings.setAllowContentAccess(false);
+        //设置WebView是否保存表单数据，默认true，保存数据。
+        webSettings.setSaveFormData(true);
+
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setUseWideViewPort(true);
+        webview.requestFocus();
+
+        //webview.setWebChromeClient(mWebChromeClient);
+        webview.setWebViewClient(mWebViewClient);
+        //缓存模式如下：
+        //LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
+        //LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
+        //LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
+        //LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
+        //webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        //webSettings.setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+    }
+
+    //辅助WebView处理Javascript的对话框,网站图标,网站标题等
+    WebChromeClient mWebChromeClient = new WebChromeClient() {
+
+        private String mTitle;
+
+        //获取Web页中的标题
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            this.mTitle = title;
+        }
+
+        //获得网页的加载进度并显示
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+
+        }
+    };
+
+    WebViewClient mWebViewClient = new WebViewClient() {
+
+        //复写shouldOverrideUrlLoading()方法，使得打开网页时不调用系统浏览器，
+        //而是在本WebView中显示
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.e("zzf",url);
+            view.loadUrl(url);
+            //startActivity(new Intent(MainActivity.this,PayActivity.class));
+            /*if (url.startsWith("app://xxx")) {
+                return true;
+            } else if (url.startsWith("https://xxx")) {
+                return true;
+            }*/
+            return true;
+        }
+
+        //开始载入页面调用的
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        //在页面加载结束时调用
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+
+        //在加载页面资源时会调用，每一个资源（比如图片）的加载都会调用一次。
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            super.onLoadResource(view, url);
+        }
+
+        @Override
+        public void onReceivedSslError(WebView webView, SslErrorHandler sslErrorHandler, SslError sslError) {
+            //handler.cancel(); 默认的处理方式，WebView变成空白页
+            //接受证书
+            sslErrorHandler.proceed();
+            //handleMessage(Message msg); 其他处理
+        }
+
+        //加载页面的服务器出现错误时（如404）调用
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            webview.setVisibility(View.GONE);
+        }
+    };
+    private void initView() {
+        tv_androidcalljs = findViewById(R.id.tv_androidcalljs);
+        tv_androidcalljsargs = findViewById(R.id.tv_androidcalljsargs);
+        tv_showmsg = findViewById(R.id.tv_showmsg);
+        webview = findViewById(R.id.webview);
+
+    }
+
+    @JavascriptInterface
+    public void jsCallAndroid(){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,PayActivity.class));
+            }
+        });
+
+    }
+
+    @JavascriptInterface
+    public void jsCallAndroidArgs(String args){
+        tv_showmsg.setText(args);
+    }
+}
