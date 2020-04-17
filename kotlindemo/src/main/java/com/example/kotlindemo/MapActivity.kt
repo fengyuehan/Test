64ca3e341c27099da6d80b1d8f7dc2394b79cfb1
zhang <981831456@qq.com>
2020-04-17 22:48:50 +0800

    kotlin   Demo

--- kotlindemo/src/main/java/com/example/kotlindemo/MapActivity.kt
+++ kotlindemo/src/main/java/com/example/kotlindemo/MapActivity.kt
@@ -7,5 +7,10 @@ class MapActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        init()
+    }
+
+    private fun init() {
+
     }
 }
