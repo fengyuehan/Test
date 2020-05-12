9c6ac99b46317d7e9b2f2a02010cfd9d861769dd
zhang <981831456@qq.com>
2020-05-12 21:59:51 +0800

    ReentrantLock

--- synchronizeddemo/src/main/java/com/example/synchronizeddemo/MainActivity.java
+++ synchronizeddemo/src/main/java/com/example/synchronizeddemo/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.synchronizeddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.example.synchronizeddemo.ReentrantLock.ReentrantLockActivity;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ReentrantLockActivity.class));
+            }
+        });
         TextView textView = findViewById(R.id.tv);
         /**
          * synchronized修饰非静态方法，用同一个实例能够保证同步
