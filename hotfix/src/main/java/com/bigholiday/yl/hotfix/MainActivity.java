625c997d006731c59332855058abdb667058a3e1
zhangzhenfeng <981831456@qq.com>
2020-09-16 14:57:03 +0800

    Thinker热修复集成

---
+++ hotfix/src/main/java/com/bigholiday/yl/hotfix/MainActivity.java
@@ -0,0 +1,26 @@
+package com.bigholiday.yl.hotfix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,1/0 +"出错了",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
