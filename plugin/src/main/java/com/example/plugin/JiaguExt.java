e2f9af74d8d4893a45237ba94048c1d5460efe8a
zhangzhenfeng <981831456@qq.com>
2020-09-29 17:53:44 +0800

    gradle

---
+++ plugin/src/main/java/com/example/plugin/JiaguExt.java
@@ -0,0 +1,67 @@
+package com.example.plugin;
+
+public class JiaguExt {
+    private String userName;
+    private String userPwd;
+    private String keyStorePath;
+    private String keyStorePass;
+    private String keyStoreKeyAlias;
+    private String keyStoreKeyAliasPwd;
+    private String jaiguToolPath;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+    public String getKeyStorePath() {
+        return keyStorePath;
+    }
+
+    public void setKeyStorePath(String keyStorePath) {
+        this.keyStorePath = keyStorePath;
+    }
+
+    public String getKeyStorePass() {
+        return keyStorePass;
+    }
+
+    public void setKeyStorePass(String keyStorePass) {
+        this.keyStorePass = keyStorePass;
+    }
+
+    public String getKeyStoreKeyAlias() {
+        return keyStoreKeyAlias;
+    }
+
+    public void setKeyStoreKeyAlias(String keyStoreKeyAlias) {
+        this.keyStoreKeyAlias = keyStoreKeyAlias;
+    }
+
+    public String getKeyStoreKeyAliasPwd() {
+        return keyStoreKeyAliasPwd;
+    }
+
+    public void setKeyStoreKeyAliasPwd(String keyStoreKeyAliasPwd) {
+        this.keyStoreKeyAliasPwd = keyStoreKeyAliasPwd;
+    }
+
+    public String getJaiguToolPath() {
+        return jaiguToolPath;
+    }
+
+    public void setJaiguToolPath(String jaiguToolPath) {
+        this.jaiguToolPath = jaiguToolPath;
+    }
+}
