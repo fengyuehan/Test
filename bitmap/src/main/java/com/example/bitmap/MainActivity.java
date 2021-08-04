8b542f560f60ec79e7cff778c302ad5efe025fd7
zhangzhenfeng <981831456@qq.com>
2021-08-04 16:29:02 +0800

    bitmap

--- bitmap/src/main/java/com/example/bitmap/MainActivity.java
+++ bitmap/src/main/java/com/example/bitmap/MainActivity.java
@@ -1,27 +1,66 @@
 package com.example.bitmap;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_decodeStream,btn_decodeResource;
+    private Button btn_decodeStream,btn_decodeResource,btn_jump,btn_jump_app;
+    private static final String pkg = "com.example.dell.myapplication";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_decodeResource = findViewById(R.id.btn_decodeResource);
         btn_decodeStream = findViewById(R.id.btn_decodeStream);
+        btn_jump = findViewById(R.id.btn_jump);
+        btn_jump_app = findViewById(R.id.btn_jump_app);
+        btn_jump_app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Otheractivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivityForResult(intent,1);
+            }
+        });
+        btn_jump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //第一种  包名+指定的activity的全路径
+               /* Intent intent = new Intent();
+                ComponentName cn = new ComponentName(pkg, "com.example.dell.myapplication.OtherActivity");
+                intent.setComponent(cn);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //startActivityForResult(intent,RESULT_OK);
+                startActivityForResult(intent,1);*/
+                //第二种，包名拉起
+                Intent intent1 = getPackageManager().getLaunchIntentForPackage(pkg);
+                //intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivityForResult(intent1,1);
+                //第三种url拉起
+                /*Intent intent = new Intent();
+                intent.setData(Uri.parse("csd://pull.csd.demo/cyn"));
+                //intent.putExtra("", "");//这里Intent当然也可传递参数,但是一般情况下都会放到上面的URL中进行传递
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivityForResult(intent,1);*/
+            }
+        });
 
         btn_decodeStream.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -77,5 +116,59 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Log.e("zzf",bitmap.getByteCount()+"-----------"+bitmap.getAllocationByteCount());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("zzf","onActivityResult");
+        /*if (requestCode == 1){
+            if (resultCode == RESULT_OK){
+                String data1 = data.getStringExtra("data");
+                Toast.makeText(this,data1,Toast.LENGTH_SHORT).show();
+            }
+        }*/
+        if(data == null){
+            Log.e("zzf","data为null");
+            return;
+        }
+        String data1 = data.getStringExtra("data");
+        Toast.makeText(this,data1,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("zzf","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("zzf","onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("zzf","onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("zzf","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("zzf","onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("zzf","onPause");
     }
 }
