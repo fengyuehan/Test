abdf1449b9685f7945211065d6ffab3fb554dfba
zhangzhenfeng <981831456@qq.com>
2021-10-09 17:02:28 +0800

    includeBuild

---
+++ version/src/main/java/BuildConfig.kt
@@ -0,0 +1,8 @@
+object BuildConfig {
+    const val compileSdkVersion = 29
+    const val buildToolsVersion = "29.0.3"
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 29
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ version/src/main/java/Testing.kt
@@ -0,0 +1,5 @@
+object Testing {
+    const val jUnit = "junit:junit:4.12"
+    const val androidJunit = "androidx.test.ext:junit:1.1.1"
+    const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
+}
